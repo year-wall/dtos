4eaeb11707937a96d69faa354e59c5bf08219adc
year-wall <year.wall.l@proton.me>
2026-01-20 20:19:30 +0800

    Create ZActArrestDto.java

---
+++ ZActArrestDto.java
@@ -0,0 +1,39 @@
+@Getter
+@Setter
+public class ZActArrestDto {
+    /** 逮捕；拘留 */
+    private String arrest;
+
+    /** 图像；影像 */
+    private String image;
+
+    /** 识别；认出 */
+    private String identify;
+
+    /** 拉丁语；拉丁人的 */
+    private String latin;
+
+    /** 管；管子；显像管 */
+    private String tube;
+
+    /** 船；小艇 */
+    private String boat;
+
+    /** 两者都不；既不…也不… */
+    private String neither;
+
+    /** 管理；经营 */
+    private String manage;
+
+    /** 生长；发展 */
+    private String grow;
+
+    /** 金字塔；角锥形 */
+    private String pyramid;
+
+    /** 龙虾；小龙虾 */
+    private String lobster;
+
+    /** 礼物；赠品 */
+    private String gift;
+}

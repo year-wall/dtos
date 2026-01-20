affe547dca18f61ddfb101abb53267bdf6b0d2c6
year-wall <year.wall.l@proton.me>
2026-01-20 20:12:30 +0800

    Create ActLensLaptopEntity.java

---
+++ ActLensLaptopEntity.java
@@ -0,0 +1,39 @@
+public class ActLensLaptopEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /** lens 字段 */
+    private String lens;
+
+    /** laptop 字段 */
+    private String laptop;
+
+    /** juice 字段 */
+    private String juice;
+
+    /** window 字段 */
+    private String window;
+
+    /** code 字段 */
+    private String code;
+
+    /** cliff 字段 */
+    private String cliff;
+
+    /** century 字段 */
+    private String century;
+
+    /** sell 字段 */
+    private String sell;
+
+    /** dose 字段 */
+    private String dose;
+
+    /** love 字段 */
+    private String love;
+
+    /** neck 字段 */
+    private String neck;
+
+    /** cool 字段 */
+    private String cool;
+}

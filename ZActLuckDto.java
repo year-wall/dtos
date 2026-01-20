34b956d1d63228787053a7443b03860c65cc559a
year-wall <year.wall.l@proton.me>
2026-01-20 20:21:19 +0800

    Create ZActLuckDto.java

---
+++ ZActLuckDto.java
@@ -0,0 +1,36 @@
+@Getter
+@Setter
+public class ZActLuckDto {
+    /** 信用；贷款 */
+    private String credit;
+
+    /** 舒适；安慰 */
+    private String comfort;
+
+    /** 木头；树木 */
+    private String wood;
+
+    /** 矩阵；格局 */
+    private String matrix;
+
+    /** 最大的；最高的 */
+    private String maximum;
+
+    /** 辩论；争论 */
+    private String debate;
+
+    /** 神经；勇气 */
+    private String nerve;
+
+    /** 海洋；广大 */
+    private String ocean;
+
+    /** 重建；恢复 */
+    private String rebuild;
+
+    /** 基础；基本的 */
+    private String basic;
+
+    /** 芹菜 */
+    private String celery;
+}

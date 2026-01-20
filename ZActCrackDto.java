d7cfe8be22cebc949504a987f8b34a302641544b
year-wall <year.wall.l@proton.me>
2026-01-20 20:20:36 +0800

    Create ZActCrackDto.java

---
+++ ZActCrackDto.java
@@ -0,0 +1,39 @@
+@Getter
+@Setter
+public class ZActCrackDto {
+    /** 破裂；裂缝；破解 */
+    private String crack;
+
+    /** 空的；空缺的；未被占用的 */
+    private String vacant;
+
+    /** 超级的；极好的 */
+    private String superField;
+
+    /** 生的；未加工的；原始的 */
+    private String raw;
+
+    /** 火焰；激情 */
+    private String flame;
+
+    /** 失事；损毁；残骸 */
+    private String wreck;
+
+    /** 页面；页 */
+    private String page;
+
+    /** 优雅的；高雅的 */
+    private String elegant;
+
+    /** 点击；咔嗒声 */
+    private String click;
+
+    /** 牙齿；齿状物 */
+    private String tooth;
+
+    /** 允许；使能够 */
+    private String allow;
+
+    /** 火箭；火箭发动机 */
+    private String rocket;
+}

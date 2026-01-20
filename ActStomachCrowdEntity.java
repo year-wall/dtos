358d45f1438a7c8fdbe4e55928af63a65ae352d4
year-wall <year.wall.l@proton.me>
2026-01-20 20:18:40 +0800

    Create ActStomachCrowdEntity.java

---
+++ ActStomachCrowdEntity.java
@@ -0,0 +1,41 @@
+@Getter
+@Setter
+public class ActStomachCrowdEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /** stomach：胃、肚子 */
+    private String stomach;
+
+    /** crowd：人群、群体 */
+    private String crowd;
+
+    /** drop：下降、掉落、滴下 */
+    private String drop;
+
+    /** kangaroo：袋鼠 */
+    private String kangaroo;
+
+    /** act：行动、行为、表演、法案 */
+    private String act;
+
+    /** meat：肉、肉类 */
+    private String meat;
+
+    /** tip：提示、小费、尖端 */
+    private String tip;
+
+    /** hybrid：混合体、混合的、杂交 */
+    private String hybrid;
+
+    /** syrup：糖浆、浓液 */
+    private String syrup;
+
+    /** divorce：离婚、分离 */
+    private String divorce;
+
+    /** under：在……之下、低于 */
+    private String under;
+
+    /** forum：论坛、讨论区 */
+    private String forum;
+}

40cb16203ffd62008fdce79276f99a60574d5921
year-wall <year.wall.l@proton.me>
2026-01-20 20:09:49 +0800

    Create ActAirBitterEntity.java

---
+++ ActAirBitterEntity.java
@@ -0,0 +1,38 @@
+ /** air：空气、气流 */
+    private String air;
+
+    /** bitter：苦的、痛苦的 */
+    private String bitter;
+
+    /** tiger：老虎 */
+    private String tiger;
+
+    /** reflect：反射、反映、思考 */
+    private String reflect;
+
+    /** jelly：果冻、胶状物 */
+    private String jelly;
+
+    /** sign：标志、符号、迹象、签名 */
+    private String sign;
+
+    /** palace：宫殿、王宫 */
+    private String palace;
+
+    /** catch：抓住、捕获、赶上 */
+    private String catchField;
+
+    /** meter:米 */
+    private String meter;
+
+    /** stadium：体育场、露天竞技场 */
+    private String stadium;
+
+    /** assist：帮助、协助 */
+    private String assist;
+
+    /** version：版本、说法 */
+    private String version;
+
+    /** limb：四肢、枝干 */
+    private String limb;

66c40dd4ab0b3c9c40a4dc5f28e0792cc9caee75
year-wall <year.wall.l@proton.me>
2026-01-20 20:22:25 +0800

    Create ZActWoodDto.java

---
+++ ZActWoodDto.java
@@ -0,0 +1,39 @@
+@Getter
+@Setter
+public class ZActWoodDto {
+    /** 木头；树木 */
+    private String wood;
+
+    /** 证明；证据 */
+    private String proof;
+
+    /** 手肘；弯头 */
+    private String elbow;
+
+    /** 老鼠；鼠标 */
+    private String mouse;
+
+    /** 点；分数；要点 */
+    private String point;
+
+    /** 刻度；比例；规模 */
+    private String scale;
+
+    /** 建议；提议 */
+    private String suggest;
+
+    /** 邪恶的；不好的 */
+    private String evil;
+
+    /** 虚拟的；模拟的 */
+    private String virtual;
+
+    /** 平衡；均衡 */
+    private String balance;
+
+    /** 拒绝；否决 */
+    private String reject;
+
+    /** 环绕；包围 */
+    private String surround;
+}

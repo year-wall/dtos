fea0eb43ea29c56d7ad32c55d4368e4823855fff
year-wall <year.wall.l@proton.me>
2026-01-20 20:13:40 +0800

    Create ActPrizeDto.java

---
+++ ActPrizeDto.java
@@ -0,0 +1,39 @@
+@Getter
+@Setter
+public class ActPrizeDto {
+    /** extra 额外的 */
+    private String extra;
+
+    /** beauty 美丽 */
+    private String beauty;
+
+    /** lottery 彩票 */
+    private String lottery;
+
+    /** crash 崩溃 / 撞击 */
+    private String crash;
+
+    /** aerobic 有氧的 */
+    private String aerobic;
+
+    /** hello 你好 */
+    private String hello;
+
+    /** aspect 方面 */
+    private String aspect;
+
+    /** exchange 交换 */
+    private String exchange;
+
+    /** birth 出生 */
+    private String birth;
+
+    /** gorilla 大猩猩 */
+    private String gorilla;
+
+    /** choose 选择 */
+    private String choose;
+
+    /** hire 雇佣 */
+    private String hire;
+}

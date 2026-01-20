e2bb4f39e30da80f874d96db217f51ac01dd9f05
year-wall <year.wall.l@proton.me>
2026-01-20 20:13:18 +0800

    Create ActPoolDto.java

---
+++ ActPoolDto.java
@@ -0,0 +1,39 @@
+@Getter
+@Setter
+public class ActPoolDto {
+    /** resource 资源 */
+    private String resource;
+
+    /** tank 坦克 / 容器 */
+    private String tank;
+
+    /** garden 花园 */
+    private String garden;
+
+    /** prepare 准备 */
+    private String prepare;
+
+    /** staff 员工 / 人员 */
+    private String staff;
+
+    /** spin 旋转 */
+    private String spin;
+
+    /** winter 冬天 */
+    private String winter;
+
+    /** protect 保护 */
+    private String protect;
+
+    /** timber 木材 */
+    private String timber;
+
+    /** shop 商店 */
+    private String shop;
+
+    /** search 搜索 */
+    private String search;
+
+    /** pet 宠物 */
+    private String pet;
+}

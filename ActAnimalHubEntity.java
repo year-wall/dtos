ffd204a86a163a4b24430d24e9a019a38be6c550
year-wall <year.wall.l@proton.me>
2026-01-20 20:10:34 +0800

    Create ActAnimalHubEntity.java

---
+++ ActAnimalHubEntity.java
@@ -0,0 +1,35 @@
+ /** animal 字段 */
+        private String animal;
+
+        /** hub 字段 */
+        private String hub;
+
+        /** pistol 字段 */
+        private String pistol;
+
+        /** mom 字段 */
+        private String mom;
+
+        /** ethics 字段 */
+        private String ethics;
+
+        /** almost 字段 */
+        private String almost;
+
+        /** favorite 字段 */
+        private String favorite;
+
+        /** motion 字段 */
+        private String motion;
+
+        /** camp 字段 */
+        private String camp;
+
+        /** oak 字段 */
+        private String oak;
+
+        /** sphere 字段 */
+        private String sphere;
+
+        /** another 字段 */
+        private String another;

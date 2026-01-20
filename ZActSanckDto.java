e65461b23a87a4a56e67e65f1c2ddf34baded16a
year-wall <year.wall.l@proton.me>
2026-01-20 20:21:41 +0800

    Create ZActSanckDto.java

---
+++ ZActSanckDto.java
@@ -0,0 +1,39 @@
+@Getter
+@Setter
+public class ZActSanckDto {
+    /** 小吃；点心 */
+    private String snack;
+
+    /** 辩论；争论 */
+    private String debate;
+
+    /** 宫殿；王宫 */
+    private String palace;
+
+    /** 培根；熏肉 */
+    private String bacon;
+
+    /** 输出；产出 */
+    private String output;
+
+    /** 错误的；假的 */
+    private String falseField; // false 为关键字，字段名改为 falseField
+
+    /** 节奏；韵律 */
+    private String rhythm;
+
+    /** 棚；小屋 */
+    private String shed;
+
+    /** 皇家的；王室的 */
+    private String royal;
+
+    /** 有用的；实用的 */
+    private String useful;
+
+    /** 真实的；真正的；真诚的 */
+    private String genuine;
+
+    /** 重力；引力 */
+    private String gravity;
+}

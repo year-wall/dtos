5930ba1e7d4888e5a44f3efc5ca8bc934f5c0493
year-wall <year.wall.l@proton.me>
2026-01-20 20:20:14 +0800

    Create ZActCancelDto.java

---
+++ ZActCancelDto.java
@@ -0,0 +1,39 @@
+@Getter
+@Setter
+public class ZActCancelDto {
+    /** 取消；终止 */
+    private String cancel;
+
+    /** 跟踪；追踪 */
+    private String track;
+
+    /** 部落；宗族 */
+    private String tribe;
+
+    /** 发烧；热情（fever 第一次出现） */
+    private String fever1;
+
+    /** 酒吧；条状物 */
+    private String bar;
+
+    /** 乌龟（陆龟） */
+    private String tortoise;
+
+    /** 动作；运动；移动 */
+    private String motion;
+
+    /** 获得；增益 */
+    private String gain;
+
+    /** 增强；提高 */
+    private String enhance;
+
+    /** 发烧；热情（fever 第二次出现） */
+    private String fever2;
+
+    /** 更喜欢；偏好 */
+    private String prefer;
+
+    /** 防御；防护；辩护 */
+    private String defense;
+}
